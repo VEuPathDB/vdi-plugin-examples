2f497261c0f32104c726964932003e52aa40baf7
Elizabeth <foxcapade@gmail.com>
2023-02-03 21:41:20 -0500

    updates

--- bash-example/scripts/import.sh
+++ bash-example/scripts/import.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env sh
 
-# Example Import Script
-#
-#     This file is an example VDI Handler Plugin Import Script.  This file is
-# written as a shell script, however, in practice, the import script may be
-# written in any language as long as it is callable from the CLI at the expected
-# path (note that additional languages will have to be installed in the
-# Dockerfile).
-#
-#     This example script demonstrates some basic components that should be
-# common to all implementations of a VDI Handler Plugin Import Script, such as
-# the exit codes, and printing warnings to STDOUT and log messages to STDERR.
-
 #
 # Exit Codes
 #
